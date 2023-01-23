fcf6c49de76b4e1d722897230a8893032d5931b5
luka <lukabarbakadze18@gmail.com>
2023-01-23 23:09:41 +0400

    first commit

--- src/components/Message.js
+++ src/components/Message.js
@@ -27,7 +27,7 @@ export default function Message(props) {
         <div className="messageContent">
             <p>{props.text}</p>
             {/* <img src="https://images.unsplash.com/photo-1537123547273-e59f4f437f1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8cnVubmluZyUyMGRvZ3xlbnwwfHwwfHw%3D&w=1000&q=80" alt="sent image" /> */}
-          {  props.image && <img onClick={() => setSomeClass(true)} className={someClass && 'scale'} src={props.image} alt="sent image" />}
+          {  props.image && <img onClick={() => setSomeClass(true)} className={someClass ? 'scale' : ''} src={props.image} alt="sent image" />}
         </div>
     </div>
   )
--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,7 @@ export default function Search() {
   return (
     <div className='search'>
       <div className='searchForm'>
-        <input type="text" placeholder='Search for friends'/>
+        <input className='search-friends' type="text" placeholder='Search for friends'/>
       </div>
       <div className="userChat mainAcc">
         <img src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="profile pic" />
